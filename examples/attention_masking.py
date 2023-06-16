637f72dccdf8c2818e7edceede5eb59974a34f19
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-06-16 04:43:06 +0100

    Simplified masking
    - Converted example into suitable format for attention
    - Removed custom value parameter and converted mask into 0 and 1s matrix. Complements attention mechanism

--- examples/attention_masking.py
+++ examples/attention_masking.py
@@ -2,17 +2,19 @@ import torch
 import torch.nn as nn
 
 
-def generate_mask(x: torch.Tensor, mask_value: int = -1e9) -> torch.Tensor:
-    """Creates a mask containing 0s and a custom value on the upper diagonal. Mask is the same size as the given input."""
+def generate_mask(x: torch.Tensor) -> torch.Tensor:
+    """Creates a mask containing 0s and 1s, where 0s are on the upper diagonal. Mask is the same size as the given input."""
     # Create a temporary tensor that matches the input size
     temp = torch.ones(x.size())
-    temp = torch.fill(temp, mask_value)  # Fill with custom value
-    return torch.triu(temp, diagonal=1)  # Convert to mask and return
+    return 1 - torch.triu(temp, diagonal=1)  # Convert to mask and return
 
 
 def mask_scores(x: torch.Tensor) -> torch.Tensor:
     """Generates a mask as the same size as the given input and automatically applies the mask to it."""
     mask = generate_mask(x)
+    print(f'Embedded data before masking:\n {x}\n')
+    x = x.masked_fill(mask == 0, -1e9)  # -1e9: not infinity but large enough!
+    print(f'Embedded data after masking:\n {x}\n')
     return torch.softmax(x + mask, dim=-1)
 
 
@@ -24,6 +26,8 @@ if __name__ == "__main__":
 
     # Generate random sample data
     sm_data = torch.randint(1, VOCAB_SIZE, (BATCH_SIZE, MAX_SEQ_LENGTH))  # 3x3 matrix
+    print(f'Data size: {sm_data.size()}')
+    print(f'Data:\n {sm_data}\n')
 
     # Embed it
     embedding = nn.Embedding(VOCAB_SIZE, EMBED_SIZE)
@@ -34,12 +38,6 @@ if __name__ == "__main__":
     masked_scores = mask_scores(embedded_data)
 
     # View outputs for data sizes and data format
-    print(f'Data size: {sm_data.size()}')
-    print(f'Data:\n {sm_data}\n')
-
-    print(f'Embedded data size: {embedded_data.size()}')
-    print(f'Embedded data:\n {embedded_data}\n')
-
     print(f'Mask size: {mask.size()}')
     print(f'Mask:\n {mask}\n')
 
--- model/mask.py
+++ model/mask.py
@@ -2,9 +2,8 @@ import torch
 import torch.nn as nn
 
 
-def generate_mask(x: torch.Tensor, mask_value: int = -1e9) -> torch.Tensor:
-    """Creates a mask containing 0s and a custom value on the upper diagonal. Mask is the same size as the given input."""
+def generate_mask(x: torch.Tensor) -> torch.Tensor:
+    """Creates a mask containing 0s and 1s, where 0s are on the upper diagonal. Mask is the same size as the given input."""
     # Create a temporary tensor that matches the input size
     temp = torch.ones(x.size())
-    temp = torch.fill(temp, mask_value)  # Fill with custom value
-    return torch.triu(temp, diagonal=1)  # Convert to mask and return
+    return 1 - torch.triu(temp, diagonal=1)  # Convert to mask and return
