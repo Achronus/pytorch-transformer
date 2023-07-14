8a5deb8c9f9f636f9a686231306b8903d98f2bab
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-07-14 04:03:33 +0100

    Removed duplicate code
    - Removed `generate_mask` function and imported from `model.mask` instead

--- examples/attention_masking.py
+++ examples/attention_masking.py
@@ -1,17 +1,11 @@
 import logging
 from model.attention import MultiHeadedAttention
+from model.mask import generate_mask
 
 import torch
 import torch.nn as nn
 
 logger = logging.getLogger('masking')
-
-
-def generate_mask(x: torch.Tensor) -> torch.Tensor:
-    """Creates a mask containing 0s and 1s, where 0s are on the upper diagonal. Mask is the same size as the given input."""
-    # Create a temporary tensor that matches the input size
-    temp = torch.ones(x.size())
-    return 1 - torch.triu(temp, diagonal=1)  # Convert to mask and return
 
 
 if __name__ == "__main__":
