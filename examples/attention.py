339a8dcc4f7fa504831fd19ad4249ed173932955
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-09-23 04:37:41 +0100

    Updated attention example
    - Changed `PositionalEncoding` to custom module variant

--- examples/attention.py
+++ examples/attention.py
@@ -5,28 +5,10 @@ Additionally, contains an example of PositionalEncoding and torch.nn.Embedding t
 
 import math
 from model.attention import MultiHeadedAttention
+from model.encoding import AbsolutePositionalEncoding
 
 import torch
 import torch.nn as nn
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, embed_size: int, max_seq_length: int) -> None:
-        """A positional encoding example from `Arjun Sarkar's Medium Blog` post
-        (https://towardsdatascience.com/build-your-own-transformer-from-scratch-using-pytorch-84c850470dcb)."""
-        super().__init__()
-
-        pe = torch.zeros(max_seq_length, embed_size)
-        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, embed_size, 2).float() * -(math.log(10000.0) / embed_size))
-
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-
-        self.register_buffer('pe', pe.unsqueeze(0))
-
-    def forward(self, x):
-        return x + self.pe[:, :x.size(1)]
 
 
 if __name__ == "__main__":
@@ -42,7 +24,7 @@ if __name__ == "__main__":
     tgt_data = torch.randint(1, TGT_VOCAB_SIZE, (BATCH_SIZE, MAX_SEQ_LENGTH))
 
     mh_attn = MultiHeadedAttention(embed_size=EMBED_SIZE, n_heads=NUM_HEADS, log_info=True)
-    pos_encode = PositionalEncoding(embed_size=EMBED_SIZE, max_seq_length=MAX_SEQ_LENGTH)  # To be refactored/replaced
+    pos_encode = AbsolutePositionalEncoding(embed_dim=EMBED_SIZE, drop_prob=0.1, max_len=MAX_SEQ_LENGTH, log_info=True)
     embedding = nn.Embedding(SRC_VOCAB_SIZE, EMBED_SIZE)
 
     encoded_input = pos_encode(embedding(src_data))
