fef2963d4bdb39e30f15adf450d68ec44742349f
Ryan Partridge <rpartridge101@yahoo.co.uk>
2023-09-23 04:51:16 +0100

    Updated normalization classes
    - Modified doc strings
    - Updated `LayerNormalization` calculation to only calculate mean and variance along the feature dimension
    - Updated parameter types
    - Added `self.device` to class

--- model/normalize.py
+++ model/normalize.py
@@ -6,13 +6,13 @@ class LayerNormalization(nn.Module):
     """
     A module dedicated to Layer Normalization formulated in the paper https://arxiv.org/abs/1607.06450.
 
-    :param feature_dims: (tuple[int, ...] | torch.size) the shape of the data to normalize
-    :param epsilon: (float, optional) a value added to the denominator for numerical stability. Default: 1e-6
-    :param device: (string, optional) name of the PyTorch CUDA device to connect to (if CUDA is available). Defaults to cuda:0
+    :param feature_dims: (int) the number of input features or channels
+    :param epsilon: (float, optional) a value added to the denominator for numerical stability. Default is `1e-6`
+    :param device: (string, optional) name of the PyTorch CUDA device to connect to (if CUDA is available). Defaults to `cpu`
     """
-    def __init__(self, feature_dims: tuple[int, ...], epsilon: float = 1e-6, device: str = 'cuda:0') -> None:
+    def __init__(self, feature_dims: int, epsilon: float = 1e-6, device: str = 'cpu') -> None:
         super().__init__()
-        self.device = torch.device(device if torch.cuda.is_available() else "cpu")
+        self.device = device
 
         self.feature_dims = feature_dims
         self.gain = nn.Parameter(torch.ones(feature_dims)).to(self.device)  # gamma
@@ -22,10 +22,10 @@ class LayerNormalization(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Applies Layer Normalization to a torch.Tensor of data."""
         x = x.to(self.device)
-        dims = [-(i + 1) for i in range(len(self.feature_dims))]  # reverse dimensions
 
-        mean = x.mean(dim=dims, keepdim=True)
-        variance = x.var(dim=dims, keepdim=True)
+        # Calculate mean and variance along feature dimension
+        mean = x.mean(dim=-1, keepdim=True)
+        variance = x.var(dim=-1, keepdim=True, unbiased=False)
         x_norm = (x - mean) / torch.sqrt(variance + self.eps)
 
         return self.gain * x_norm + self.bias  # scale and shift
@@ -35,15 +35,15 @@ class ResidualAndLayerNorm(nn.Module):
     """
     Performs an intermediate step between each module in the Transformer.
 
-    :param feature_dims: (tuple[int, ...] | torch.size) the shape of the data to normalize
-    :param drop_prob: (float, optional) the neuron dropout probability. Defaults to 0.1
-    :param epsilon: (float, optional) a value added to the normalization for numerical stability. Default: 1e-6
-    :param device: (string, optional) name of the PyTorch CUDA device to connect to (if CUDA is available). Defaults to cuda:0
+    :param feature_dims: (int) the number of input features or channels
+    :param drop_prob: (float, optional) the neuron dropout probability. Defaults to `0.1`
+    :param epsilon: (float, optional) a value added to the normalization for numerical stability. Default: `cpu`
+    :param device: (string, optional) name of the PyTorch CUDA device to connect to (if CUDA is available). Defaults to `cpu`
     """
-    def __init__(self, feature_dims: tuple[int, ...], drop_prob: float = 0.1,
-                 epsilon: float = 1e-6, device: str = 'cuda:0') -> None:
+    def __init__(self, feature_dims: int, drop_prob: float = 0.1,
+                 epsilon: float = 1e-6, device: str = 'cpu') -> None:
         super().__init__()
-        self.device = torch.device(device if torch.cuda.is_available() else "cpu")
+        self.device = device
         self.norm = LayerNormalization(feature_dims=feature_dims, epsilon=epsilon).to(self.device)
         self.dropout = nn.Dropout(p=drop_prob)
 
